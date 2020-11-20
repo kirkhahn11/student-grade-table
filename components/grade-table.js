16143d324df76d7aa8b17c4dd163a784b58839af
Kirk Hahn <kirkhahn11@yahoo.com>
2020-11-20 05:47:33 +0000

    Completed user can view all grades

---
+++ components/grade-table.js
@@ -0,0 +1,19 @@
+class GradeTable {
+  constructor(tableElement) {
+    this.tableElement = tableElement
+  }
+  updateGrades(grades) {
+    var tbody = this.tableElement.querySelector('tbody')
+    for(var i = 0; i < grades.length; i++){
+      var tr = document.createElement('tr')
+      var tdName = document.createElement('td')
+      tdName.textContent = grades[i].name
+      var tdCourse = document.createElement('td')
+      tdCourse.textContent = grades[i].course
+      var tdGrade = document.createElement('td')
+      tdGrade.textContent = grades[i].grade
+      tr.append(tdName, tdCourse, tdGrade)
+      tbody.append(tr)
+    }
+  }
+}
