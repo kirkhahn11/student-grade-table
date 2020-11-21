7462ab16e9dc30f68a40812541a916ccabd49b3b
Kirk Hahn <kirkhahn11@yahoo.com>
2020-11-21 22:37:32 +0000

    Completed enhance the gradetable component

--- components/grade-table.js
+++ components/grade-table.js
@@ -1,22 +1,40 @@
 class GradeTable {
-  constructor(tableElement) {
+  constructor(tableElement, noGradesElement) {
     this.tableElement = tableElement
+    this.noGradesElement = noGradesElement
   }
   updateGrades(grades) {
     var tbody = this.tableElement.querySelector('tbody')
+    if(!tbody.lastChild){
+      noGrade.classList.remove('d-none')
+    }
     while(tbody.lastChild){
       tbody.removeChild(tbody.lastChild)
     }
     for(var i = 0; i < grades.length; i++){
-      var tr = document.createElement('tr')
-      var tdName = document.createElement('td')
-      tdName.textContent = grades[i].name
-      var tdCourse = document.createElement('td')
-      tdCourse.textContent = grades[i].course
-      var tdGrade = document.createElement('td')
-      tdGrade.textContent = grades[i].grade
-      tr.append(tdName, tdCourse, tdGrade)
-      tbody.append(tr)
+      tbody.append(this.renderGradeRow(grades[i], this.deleteGrade))
     }
+  }
+  onDeleteClick(deleteGrade){
+    this.deleteGrade = deleteGrade
+  }
+  renderGradeRow(data, deleteGrade){
+    var tr = document.createElement('tr')
+    var tdName = document.createElement('td')
+    tdName.textContent = data.name
+    var tdCourse = document.createElement('td')
+    tdCourse.textContent = data.course
+    var tdGrade = document.createElement('td')
+    tdGrade.textContent = data.grade
+    var tdDelete = document.createElement('td')
+    var deleteButton = document.createElement('button')
+    var deleteButtonText = document.createTextNode('Delete')
+    deleteButton.addEventListener('click', function(){
+      deleteGrade(data.id)
+    })
+    deleteButton.append(deleteButtonText)
+    tdDelete.append(deleteButton)
+    tr.append(tdName, tdCourse, tdGrade, tdDelete)
+    return tr
   }
 }
--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 var tableElement = document.querySelector('.table')
 var headerElement = document.querySelector('.header')
 var formElement = document.querySelector('form')
+var noGrade = document.querySelector('.no-grades')
 
 var gradeForm = new GradeForm(formElement)
 
 var pageHeader = new PageHeader(headerElement)
 
-var gradeTable = new GradeTable(tableElement)
+var gradeTable = new GradeTable(tableElement, noGrade)
 
 var app = new App(gradeTable, pageHeader, gradeForm)
 
